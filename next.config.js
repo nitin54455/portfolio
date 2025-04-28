f5c0950a1c297cfa44ad5a6da2b1a6b22e10b6fb
nitin singh <119619544+nitin54455@users.noreply.github.com>
2025-04-29 00:43:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['your-photo.jpg'], // Add this if you're using external image URLs
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
